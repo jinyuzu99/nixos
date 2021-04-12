bba5644ee1ca13982d97f8608c3233e8501043ca
sayo melu <hi.sayo@outlook.com>
2021-04-12 14:42:33 +0800

    use gnome3.excludePackages

--- module/gnome.nix
+++ module/gnome.nix
@@ -1,7 +1,14 @@
 { config, pkgs, ... }:
 {
-	services = {
-		xserver.desktopManager.gnome3.enable = true;
-		gnome3.core-utilities.enable = false;
-	};
+	services.xserver.desktopManager.gnome3.enable = true;
+	environment.gnome3.excludePackages = with pkgs; [
+		gnome3.gnome-contacts
+		gnome3.gnome-clocks
+		gnome3.gnome-maps
+		gnome3.gnome-weather
+		gnome3.cheese
+		gnome3.geary
+		gnome3.simple-scan
+		gnome-photos
+	];
 }
