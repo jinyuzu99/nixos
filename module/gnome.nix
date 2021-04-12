bfdeab9c3ae048338609197e0c25643fb5eeaf5d
sayo melu <hi.sayo@outlook.com>
2021-04-12 14:48:22 +0800

    add gnome3.excludePackages item

--- module/gnome.nix
+++ module/gnome.nix
@@ -2,6 +2,8 @@
 {
 	services.xserver.desktopManager.gnome3.enable = true;
 	environment.gnome3.excludePackages = with pkgs; [
+		gnome3.gnome-calculator
+		gnome3.gnome-calendar
 		gnome3.gnome-contacts
 		gnome3.gnome-clocks
 		gnome3.gnome-maps
@@ -9,6 +11,8 @@
 		gnome3.cheese
 		gnome3.geary
 		gnome3.simple-scan
+		gnome3.yelp
+		gnome-connections
 		gnome-photos
 	];
 }
